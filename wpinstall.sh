8011faca1f91880b5d9353a947039fa7562ffecd
Harry Jackson <harryjackson1221@gmail.com>
2017-08-14 11:39:37 -0400

    Update wpinstall.sh
    Added info to create database using mysql commands, still testing and working out bugs

--- wpinstall.sh
+++ wpinstall.sh
@@ -22,11 +22,39 @@ cp "$CWD"/wp-config-sample.php "$CWD"/wp-config.php
 #Time to set a username and password randomly, and print out to suggest using
 PASSWDDB="$(date | sha256sum | base64 | head -c 8 )"
 DBNAME="$(date | md5sum | head -c 8 )"
+DBUNAME="$()"
 
 #Username on CentOS 7 w/ cPanel - need to devise a way to ask for the file owner/user, dbhost dbname dbuser and pass
 #p=$(pwd | cut -d/ -f3)
-# (in progress) mysql create $DBNAME; 
-# (in progress) mysql create-user $DBNAME $PASSWDDB;
+echo "WP Database Name: "
+read -e dbname
+echo "WP Database User: "
+read -e dbuser
+echo "WP Database Password: "
+read -s dbpass
+echo "WP Database Table Prefix [numbers, letters, and underscores only] (Enter for default 'wp_'): "
+read -e dbtable
+        dbtable=${dbtable:-wp_}
+echo "Do you need to setup new MySQL database? (y/n)"
+read -e setupmysql
+if [ "$setupmysql" == y ] ; then
+		echo "============================================"
+		echo "Setting up the database."
+		echo "============================================"
+		#login to MySQL, add database, add user and grant permissions
+		dbsetup="create database $dbname;GRANT ALL PRIVILEGES ON $dbname.* TO $dbuser@$mysqlhost IDENTIFIED BY '$dbpass';FLUSH PRIVILEGES;"
+		mysql -u $mysqluser -p $mysqlpass -e "$dbsetup"
+		if [ $? != "0" ]; then
+			echo "============================================"
+			echo "[Error]: Database creation failed. Aborting."
+			echo "============================================"
+			exit 1
+		fi
+fi
+
+#login to MySQL, add database, add user and grant permissions
+dbsetup="create database $dbname;GRANT ALL PRIVILEGES ON $dbname.* TO $dbuser@$mysqlhost IDENTIFIED BY '$dbpass';FLUSH PRIVILEGES;"
+mysql -u $mysqluser -p$mysqlpass -e "$dbsetup"
 
 #autop=$(echo $p | cut -c1-8);
 
@@ -42,6 +70,9 @@ DBNAME="$(date | md5sum | head -c 8 )"
 #echo "DB privleges granted"
 
 # Credit to Daniel K for helping with this one, the old way used grep and cut then sed
+# DBNAME="test"; sed -nr "s/define\(([\"'])DB_NAME\1,\s*([\"']).*\2/define(\1DB_NAME\1, \2${DBNAME}\2/gp" wp-config.php 
+#define('DB_NAME', 'test');
+#
 sed -r \
     "
         s/define\(([\"'])DB_(NAME|USER)\1,\s*([\"']).*\2/define(\1DB_NAME\1, \2${DBNAME}\2/g;
